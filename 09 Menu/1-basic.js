a95892b6ea51f80c86f95b433a759cb95a332809
Ashutosh Chaudhary <136228533+newashutosh07@users.noreply.github.com>
2024-06-08 16:41:52 +0545

    Menu(Incomplete)

--- 09 Menu/1-basic.js
+++ 09 Menu/1-basic.js
@@ -74,46 +74,26 @@ const menu = [
 ];
 
 const sectionCenter = document.querySelector(".section-center");
-const filterBtns = document.querySelectorAll(".filter-btn");
 
-//load items
-window.addEventListener("DOMContentLoaded", () => {
-  displayMenuItems(menu);
-});
+window.addEventListener("DOMContentLoaded", function () {
+  let displayMenu = menu.map(function (item) {
+    // console.log(item);
 
-//filter items
-filterBtns.forEach(function (btn) {
-  btn.addEventListener("click", function (e) {
-    // console.log(e.currentTarget.dataset.id);
-    const category = e.currentTarget.dataset.id;
-    const menuCategory = menu.filter(function (menuItem) {
-      if (menuItem.category == category) {
-        return menuItem;
-      }
-    });
-    if (category == "all") {
-      displayMenuItems(menu);
-    } else {
-      displayMenuItems(menuCategory);
-    }
-  });
-});
-
-function displayMenuItems(menuItems) {
-  let displayMenu = menuItems.map(function (item) {
     return `<article class="menu-item">
-                <img src=${item.img} class="photo" alt="menu-item">
-                <div class="item-info">
-                    <header>
-                        <h4>${item.title}</h4>
-                        <h4 class="price">$${item.price}</h4>
-                    </header>
-                    <p class="item-text">
-                    ${item.desc}
-                    </p>
-                </div>
-            </article>`;
+          <img src=${item.img} alt=${item.title} class="photo" />
+          <div class="item-info">
+            <header>
+              <h4>${item.title}</h4>
+              <h4 class="price">$${item.price}</h4>
+            </header>
+            <p class="item-text">
+              ${item.desc}
+            </p>
+          </div>
+        </article>`;
   });
   displayMenu = displayMenu.join("");
+  console.log(displayMenu);
+
   sectionCenter.innerHTML = displayMenu;
-}
+});
